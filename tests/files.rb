2598c56c65b3c71ce2d84f08b67ffee474452961
Jonathan Gabel <hello@jonathangabel.com>
2013-07-20 12:39:05 -0400

    freeze dry method

--- tests/files.rb
+++ tests/files.rb
@@ -10,7 +10,7 @@ class Files < Test::Unit::TestCase
   end
 
   def teardown
-    # FileUtils.rm_r @dir if File.exists?(@dir)
+    FileUtils.rm_r @dir if File.exists?(@dir)
   end
 
   def test_writing_and_reading_a_file
@@ -50,7 +50,7 @@ class Files < Test::Unit::TestCase
   class YamlMammal
     attr_accessor :furry, :name
 
-    def initialize(params)
+    def initialize(params={})
       @name = params[:name]
       @furry = params[:furry]
     end
@@ -60,7 +60,7 @@ class Files < Test::Unit::TestCase
       YamlMammal.new params
     end
 
-    def self.init_yamls( file )
+    def self.init_yanimals( file )
       zoo = []
       File.open( file ) do |yf|
         YAML.load_documents( yf ) do |params|
@@ -70,30 +70,70 @@ class Files < Test::Unit::TestCase
       zoo
     end
 
+    # record the state of all instance variables as a hash
+    def freeze_dry
+      freeze
+
+      record = {}
+      state = instance_variables
+      state.each do |attr|
+        key = attr[1..-1].to_sym
+        val = instance_variable_get attr
+        record[key] = val
+      end
+
+      record
+    end
+
     def store_as_yaml( file )
-      record = { name: @name, furry: @furry }
+      record = freeze_dry
       File.open( file,'a' ) do |f|
         f.puts YAML::dump( record )
       end
     end
   end
 
-  def test_writing_and_reading_a_yaml_object
+  def test_a_class_that_writes_and_reads_an_instance_to_file
 
     file = File.join( @dir, "yaml_mammal.txt")
+
+    pig = YamlMammal.new( name:'Pig', furry: false )
+    pig.store_as_yaml file
+
+    beast = YamlMammal.init_from_yaml file
+
+    assert_equal pig.name, beast.name
+  end
+
+  def test_the_freeze_dry_method
+    pig = YamlMammal.new( name:'Pig', furry: false )
+    iced_pig = pig.freeze_dry
+
+    file = File.join( @dir, "yaml_mammal.txt")
+    pig.store_as_yaml file
+    params = YAML.load_file(file)
+
+    expected_hash = { :name=>"Pig", :furry=>false }
+    assert_equal expected_hash, iced_pig
+
+    assert_equal params, iced_pig
+  end
+
+  def test_a_class_that_reads_its_members_from_file
+
+    file = File.join( @dir, "yaml_mammals.txt")
 
     pig = YamlMammal.new( name:'Pig', furry: false )
     bear = YamlMammal.new( name:'Bear', furry: true )
     pig.store_as_yaml file
     bear.store_as_yaml file
 
-    beast = YamlMammal.init_from_yaml file
+    pig.freeze_dry
 
-    assert_equal pig.name, beast.name
+    yanimals = YamlMammal.init_yanimals file
 
-    animals = YamlMammal.init_yamls file
-
-    assert_equal "Bear", animals[1].name
+    assert_equal false, yanimals[0].furry
+    assert_equal "Bear", yanimals[1].name
   end
 end
 
